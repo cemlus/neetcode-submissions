37933c1daed561982f8c6ffa5bbf09372bd899f4
siddhant <nerdycoolboi69@gmail.com>
2026-04-25 14:38:45 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    double power(double x, long n){
+        if(n == 0) return 1;
+
+        double half = power(x, n/2);
+
+        if(n % 2 == 0){
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+
+    double myPow(double x, int n) {
+        if(x == 0) return 0;
+        if(x == 1) return 1;
+
+        long num = n;
+        if(num < 0){
+            num = -num;
+            x = 1/x;
+        }
+        return power(x, num);
+    }
+};
