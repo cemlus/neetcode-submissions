b5c1d3a7e70fe515ea4c4c1d3eac763a2770193e
siddhant <nerdycoolboi69@gmail.com>
2026-04-30 21:17:27 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int lo = 0, hi = matrix.size() - 1;
+        while(lo < hi){
+            int mid = lo + (hi - lo + 1) / 2;
+            if(target >= matrix[mid][0]){
+                lo = mid;           // this is because the target element can belong to the same row in which the starting 
+                                    // element is small
+            } else {
+                hi = mid - 1;       // if the target element is lesser than the starting element of the row then it for sure    
+                                    // cannot belong to the same row
+            }
+        }
+        int row = lo;
+        cout << row << endl;
+        lo = 0;
+        hi = matrix[row].size() - 1;
+
+
+        while(lo < hi){
+            int mid = lo + (hi - lo) / 2;
+            if(target > matrix[row][mid]){
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        int col = lo;
+        cout << col << endl;
+
+        if(matrix[row][col] == target){
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
