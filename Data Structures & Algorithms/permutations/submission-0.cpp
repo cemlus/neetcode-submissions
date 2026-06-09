8cecdf6e5d9cba4b7ab7a2ff0dd21b457cf9a824
siddhant <nerdycoolboi69@gmail.com>
2026-06-09 18:44:49 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   public:
+    void generate(vector<vector<int>>& ans, vector<int>& nums, vector<int>& curr,
+                  vector<int>& freq) {
+        if (curr.size() == nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            // only allow unmarked elements
+            if (freq[i] == 0) {
+                curr.push_back(nums[i]);
+                freq[i] = 1;        // mark the element
+                generate(ans, nums, curr, freq);
+                curr.pop_back();    // unmark and pop it from the place it took
+                freq[i] = 0;
+            }
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        vector<int> freq(nums.size(), 0);
+        generate(ans, nums, curr, freq);
+        return ans;
+    }
+};
