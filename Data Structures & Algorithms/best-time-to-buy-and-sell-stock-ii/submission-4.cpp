60aba34162c35fd6794aa351dab407726ff13a79
siddhant <nerdycoolboi69@gmail.com>
2026-06-15 17:58:36 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-4

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-4.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+        // base case
+        dp[n][0] = dp[n][1] = 0;
+
+        // recursion 0 -> n
+        // tabulation n -> 0
+        for (int idx = n - 1; idx >= 0 ; idx--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if (buy) {
+                    profit = max(-prices[idx] + dp[idx + 1][0],
+                                 0 + dp[idx + 1][1]);
+                } else {
+                    profit = max(prices[idx] + dp[idx + 1][1],
+                                 0 + dp[idx + 1][0]);
+                }
+                dp[idx][buy] = profit;
+            }
+        }
+
+        return dp[0][1];
+    }
+};
